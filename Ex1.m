b501d644b45ae1861acda7cb2ebcc43b5c98cafe
Frederico Vaz <fredvaz8@gmail.com>
2018-10-14 19:06:44 +0100

    Ex1 Done

--- Ex1.m
+++ Ex1.m
@@ -13,59 +13,60 @@ disp(' ')
 
 
 %% Sistema de coordenadas INICIAL do objecto no mundo localizado em:
-wTobject = [ 0 -1  0  5;
-             0  0 -1 -2;
-             1  0  0  3;
-             0  0  0  1 ];
+
+Tw_0 = [ 0 -1  0  5;
+         0  0 -1 -2;
+         1  0  0  3;
+         0  0  0  1 ];
+         
          
 %% Sequência de Movimentos Individual
 
 % 1) Rotação de 30º sobre o eixo OX do sistema de coordenadas World:
 
-wTo_1 = trans(0, 0, 30, [0 0 0]') * wTobject;
+T0_1 = trans(0, 0, 30, [0 0 0]') * Tw_0; % Pre-Multiplicação 
 
 
 % 2) Deslocação de 3 unidades sobre o eixo OZ do atual sistema de coordenadas object:
 
-wTo_2 = trans(0, 0, 0, [0 0 3]') * wTo_1;
+T0_2 = T0_1 * trans(0, 0, 0, [0 0 3]'); % Pós-Multiplicação 
 
 
-% 3) Rotação de -45º sobre o eixo [-1 -1 1] do sistema de coordenadas object inicial: 
+% 3) Rotação de -45º sobre o eixo [1 -1 1] do sistema de coordenadas object inicial: 
 
-% Eixo 
+% Eixo (assumindo que o eixo está no referencial 1)
 phi = deg2rad(-45);
-r = [-1 -1 -1];
+r = [1 -1 1];
 
 R = vectorRot(r, phi);
 
-wTo_3 = [ R [0 0 0 ]'; [0 0 0 1] ] * wTobject;
+
+T2_3 = [ R [0 0 0 ]'; [0 0 0 1] ] * T0_2;
 
 
 % 4) Rotação de 90º do sistema de coordenadas World sobre o seu próprio eixo OZ: (semelhante ao 1) )
 
-wTo_4 = trans(90, 0, 0, [0 0 0]') * wTo_2;
+T3_4 = trans(90, 0, 0, [0 0 0]') * T2_3; % Pre-Multiplicação 
          
 
 
 %% Animação do Objecto
+FPS = 10;
 
 figure(3);
-tranimate(wTobject, wTo_1, 'rgb', 'axis', [0 10 -10 10 0 10]);
+trplot(Tw_0,'rgb', 'axis', [-10 10 -10 10 -10 10]);
 hold on
-tranimate(wTo_1, wTo_2, 'rgb', 'axis', [0 10 -10 0 10 10]);
+tranimate(Tw_0, T0_1, 'rgb', 'axis', [-10 10 -10 10 -10 10],'fps', FPS);
 hold on
-tranimate(wTo_2, wTo_4, 'rgb', 'axis', [0 10 -10 0 10 10]);
+tranimate(T0_1, T1_2, 'rgb', 'axis', [-10 10 -10 10 -10 10],'fps', FPS);
 hold on
-
-
-
-
-%wTo_ = wTo_3 * trans(0, 0, 30, [0 0 3]');
-%%
-%wTo_ = wTo_3 * wTobject;
-
-tranimate(wTobject, wTo_3, 'rgb', 'axis', [0 10 -10 0 -5 10]);
-
+% Eixo 
+plot3([Tw_0(1,4) Tw_0(1,4)+1], [Tw_0(2,4) Tw_0(2,4)-1], [Tw_0(3,4) Tw_0(3,4)+1], 'k-');
+hold on
+tranimate(T1_2, T2_3, 'rgb', 'axis', [-10 10 -10 10 -10 10],'fps', FPS);
+hold on
+tranimate(T2_3, T3_4, 'rgb', 'axis', [-10 10 -10 10 -10 10],'fps', FPS);
+hold on
 
 
 
